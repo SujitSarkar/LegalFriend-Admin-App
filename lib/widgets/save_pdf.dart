bf7b1f10a7879ae8e8e8c624f0b947ea5c9648ce
Sujit Sarkar <snigdho532@gmail.com>
2021-12-08 12:46:16 +0600

    pdf bug fixing

--- lib/widgets/save_pdf.dart
+++ lib/widgets/save_pdf.dart
@@ -6,6 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:syncfusion_flutter_pdf/pdf.dart' as syncfusion;
 import 'notification_widget.dart';
 
 class SavePDF{
@@ -27,7 +28,6 @@ class SavePDF{
         fontWeight: pw.FontWeight.normal,
         color: PdfColors.black
     );
-
     pdf.addPage(
       pw.MultiPage(
           pageFormat: PdfPageFormat.a4,
@@ -242,10 +242,53 @@ class SavePDF{
 
     Directory?  downloadsDirectory = await DownloadsPathProvider.downloadsDirectory;
     final file = File("${downloadsDirectory!.path}/${DateTime.now().millisecondsSinceEpoch}.pdf");
-    await file.writeAsBytes(await pdf.save());
+    await file.writeAsBytes(await pdf.save(),flush: true);
     closeLoadingDialog(context);
     showToast('Saved in Download Folder');
     Navigator.pop(context);
+  }
+
+
+
+  static Future<void> syncfusionPDF(List<BodliKhanaModel> dataList, String title,BuildContext context)async{
+    final syncfusion.PdfDocument pdfDocument = syncfusion.PdfDocument();
+    final page = pdfDocument.pages.add();
+
+    syncfusion.PdfGrid grid = syncfusion.PdfGrid();
+
+    grid.columns.add(count: 3);
+    grid.headers.add(1);
+
+    syncfusion.PdfGridRow header = grid.headers[0];
+    header.cells[0].value=Variables.dayraNo;
+    header.cells[1].value=Variables.crMamlaNo;
+    header.cells[2].value=Variables.pokkhogonerNam;
+    header.cells[3].value=Variables.porobortiTarikh;
+    header.cells[4].value=Variables.bicaricAdalot;
+    header.cells[5].value=Variables.amoliAdalot;
+    header.cells[6].value=Variables.mamlarDhoron;
+    header.cells[7].value=Variables.boiNo;
+    header.cells[8].value=Variables.jojCourt;
+
+    syncfusion.PdfGridRow row = grid.rows.add();
+    row.cells[0].value=Variables.dayraNo;
+    row.cells[1].value=Variables.crMamlaNo;
+    row.cells[2].value=Variables.pokkhogonerNam;
+    row.cells[3].value=Variables.porobortiTarikh;
+    row.cells[4].value=Variables.bicaricAdalot;
+    row.cells[5].value=Variables.amoliAdalot;
+    row.cells[6].value=Variables.mamlarDhoron;
+    row.cells[7].value=Variables.boiNo;
+    row.cells[8].value=Variables.jojCourt;
+
+    grid.draw(page: pdfDocument.pages.add(),bounds: const Rect.fromLTWH(0,0,0,0));
+
+    List<int> bytes = pdfDocument.save();
+    pdfDocument.dispose();
+
+    Directory?  downloadsDirectory = await DownloadsPathProvider.downloadsDirectory;
+    final file = File('${downloadsDirectory!.path}/ismail.pdf');
+    await file.writeAsBytes(bytes,flush: true);
   }
 
 }
